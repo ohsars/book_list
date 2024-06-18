a8322d3290f92a70849aabcca5b7a28bf7d9605f
Marvelous Osakpolor <efebeosas@gmail.com>
2024-06-18 13:58:46 +0100

    Made few comments on errors encountered

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'http://openlibrary.org/people/george08';
+
 // http://openlibrary.org/people/george08/lists/OL97L/seeds.json
 
 export const getLists = async () => {
@@ -13,16 +14,19 @@ export const getList = async (listId) => {
   return response.data;
 };
 
+// For some Reasons these endpoint aren't connecting, err 302
 export const createList = async (listData) => {
   const response = await axios.post(`${API_URL}/lists`, listData);
   return response.data;
 };
 
+// Cors Error gotten here 
 export const updateList = async (listId, listData) => {
   const response = await axios.put(`${API_URL}/lists/${listId}`, listData);
   return response.data;
 };
 
+// Can't perfom these action originally according to the website documentation 
 export const deleteList = async (listId) => {
   const response = await axios.delete(`${API_URL}/lists/${listId}`);
   return response.data;
